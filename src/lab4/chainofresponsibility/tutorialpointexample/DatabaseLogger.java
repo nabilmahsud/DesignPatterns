4bcb56796428ae544e836ebc1598217c7d5c4984
Nabil <cleo.master0000@gmail.com>
2023-09-28 09:24:09 +0500

    Add responsibility pattern

---
+++ src/lab4/chainofresponsibility/tutorialpointexample/DatabaseLogger.java
@@ -0,0 +1,13 @@
+package lab4.chainofresponsibility.tutorialpointexample;
+
+public class DatabaseLogger extends AbstractLogger {
+
+    public DatabaseLogger(int level) {
+        this.level = level;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("Database Console::Logger: " + message);
+    }
+}
---
+++ src/lab4/chainofresponsibility/tutorialpointexample/Main.java
@@ -0,0 +1,39 @@
+package lab4.chainofresponsibility.tutorialpointexample;
+
+public class Main {
+
+    private static AbstractLogger getChainOfLoggers(AbstractLogger... loggers) {
+
+        AbstractLogger errorLogger = new ErrorLogger(AbstractLogger.ERROR);
+        AbstractLogger fileLogger = new FileLogger(AbstractLogger.DEBUG);
+        AbstractLogger consoleLogger = new ConsoleLogger(AbstractLogger.INFO);
+        AbstractLogger databaseLogger = new DatabaseLogger(AbstractLogger.WARNING);
+
+        for (var i = 0; i < loggers.length; i++)
+            if(i != loggers.length - 1)
+                loggers[i].setNextLogger(loggers[i+1]);
+
+
+//        errorLogger.setNextLogger(fileLogger);
+//        fileLogger.setNextLogger(consoleLogger);
+//        consoleLogger.setNextLogger(databaseLogger);
+
+        return loggers[0];
+    }
+
+
+    public static void main(String[] args) {
+        var loggerChain = getChainOfLoggers(new ErrorLogger(AbstractLogger.ERROR),
+                                                        new FileLogger(AbstractLogger.DEBUG),
+                                                        new ConsoleLogger(AbstractLogger.INFO),
+                                                        new DatabaseLogger(AbstractLogger.WARNING));
+
+        var loggerChain2 = getChainOfLoggers(new ErrorLogger(AbstractLogger.ERROR),
+                                                        new DatabaseLogger(AbstractLogger.WARNING),
+                                                        new ConsoleLogger(AbstractLogger.INFO));
+
+        loggerChain2.logMessage(AbstractLogger.DEBUG,
+                "This is an debug level information.");
+
+    }
+}
