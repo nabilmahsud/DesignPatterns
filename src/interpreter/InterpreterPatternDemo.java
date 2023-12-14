0369409993a40eacaaa1a7ff1c0d32be9f0afd5d
Nabil <cleo.master0000@gmail.com>
2023-12-14 09:42:29 +0500

    Add files via upload

--- src/interpreter/InterpreterPatternDemo.java
+++ src/interpreter/InterpreterPatternDemo.java
@@ -21,14 +21,20 @@ public class InterpreterPatternDemo {
       return new AndExpression(julie, married);		
    }
    
+      public static Expression isSon(){
+      Expression stuart = new TerminalExpression("Stuart");
+      Expression robert = new TerminalExpression("Robert");
+      return new AndExpression(stuart, robert);		
+   }
+   
 
    public static void main(String[] args) {
       Expression isMale = getMaleExpression();
       Expression isMarriedWoman = getMarriedWomanExpression();
-      Expression isSon = new SonExpression("Robert", "Stuart");
+      Expression isSon = isSon();
 
       System.out.println("John is male? " + isMale.interpret("John"));
       System.out.println("Julie is a married women? " + isMarriedWoman.interpret("Married Julie"));
-      System.out.println("Stuart is a son of Robert? " + isSon.interpret("Robert Stuart"));
+      System.out.println("Stuart is a son of Mike? " + isSon.interpret("Stuart Robert"));
    }
 }
